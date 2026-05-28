670fdd6c37cd4bf3fea1024c673b4c4dcc730ad7
Amanda <amandafrolini6@gmail.com>
2026-05-28 14:21:05 -0300

    primeiro commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace AppMeuCombustivel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            double preco_gasolina = Convert.ToDouble(gasolina.Text);
+            double preco_etanol = Convert.ToDouble(etanol.Text);
+            string msg = "";
+
+            if(preco_etanol > (preco_gasolina * 0.70)){
+                msg = "Compensa a gasolina";
+            }
+            else
+            {
+                msg = "Compensa o etanol";
+            }
+            DisplayAlertAsync("Resultado:", msg, "OK");
+        }
+    }
+}
